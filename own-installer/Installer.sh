639f6cf29596a5afb5c9d479431bbe616a0d92a3
BriefDragon <140400090+BriefDragon@users.noreply.github.com>
2024-08-21 23:10:10 +0200

    own installer
    my new Installer

---
+++ own-installer/Installer.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+echo "                                                                                                        "
+echo "                                                                                                        "
+echo "  /$$$$$$                      /$$             /$$$$$$                       /$$               /$$ /$$"
+echo "  /$$__  $$                    | $$            |_  $$_/                      | $$              | $$| $$"
+echo " | $$  \ $$  /$$$$$$   /$$$$$$$| $$$$$$$         | $$   /$$$$$$$   /$$$$$$$ /$$$$$$    /$$$$$$ | $$| $$"
+echo " | $$$$$$$$ /$$__  $$ /$$_____/| $$__  $$        | $$  | $$__  $$ /$$_____/|_  $$_/   |____  $$| $$| $$"
+echo " | $$__  $$| $$  \__/| $$      | $$  \ $$        | $$  | $$  \ $$|  $$$$$$   | $$      /$$$$$$$| $$| $$"
+echo " | $$  | $$| $$      | $$      | $$  | $$        | $$  | $$  | $$ \____  $$  | $$ /$$ /$$__  $$| $$| $$"
+echo " | $$  | $$| $$      |  $$$$$$$| $$  | $$       /$$$$$$| $$  | $$ /$$$$$$$/  |  $$$$/|  $$$$$$$| $$| $$"
+echo " |__/  |__/|__/       \_______/|__/  |__/      |______/|__/  |__/|_______/    \___/   \_______/|__/|__/"  
+cat /run/archiso/bootmnt/own-installer/art-input.txt
+echo 
+#Partitionenbeginn jetzt
+lsblk
+read -p "Bitte gebe deine Root-Partition ein wie im folgenden Beispiel (z.b. /dev/sda3). Das Kannst du mit der liste über dir herrauslesen. Sei dir bitte sicher, dass es richtig ist und notier dir auch ruhig die anderen Partitionen." Haupt
+    mkfs.ext4 "$Haupt"
+    mount "$Haupt" /mnt 
+# Swap 
+read -p "Bitte Swap-partition in dem Schemata von eben eingeben." Tausch
+    mkswap "$Tausch"
+    swapon "$Tausch"
+# Boot
+read -p "Bitte jetzt als letztes die Boot Partition auch im selben Schema." Start
+    mkfs.fat -F 32 "$Start"
+    mkdir -p /mnt/boot/efi
+    mount "$Start" /mnt/boot/efi
+#ende der Partitionen ding 
+#Programme 
+cat /run/archiso/bootmnt/own-installer/art-programs.txt 
+pacstrap -K /mnt base linux linux-firmware grub efibootmgr nano networkmanager neofetch sof-firmware base-devel git sudo 
+genfstab /mnt > /mnt/etc/fstab
+cp /run/archiso/bootmnt/own-installer /mnt/root
+arch-chroot /mnt /bin/bash /root/own-installer/chroot-teil.sh
+#ende dieses Teils 
---
+++ own-installer/chroot-teil.sh
@@ -0,0 +1,94 @@
+#! /bin/bash
+echo "            /$$                                               /$$     "
+echo "           | $$                                              | $$     "
+echo "   /$$$$$$$| $$$$$$$           /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$   "
+echo "  /$$_____/| $$__  $$ /$$$$$$ /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/   "
+echo " | $$      | $$  \ $$|______/| $$  \__/| $$  \ $$| $$  \ $$  | $$     "
+echo " | $$      | $$  | $$        | $$      | $$  | $$| $$  | $$  | $$ /$$ "
+echo " |  $$$$$$$| $$  | $$        | $$      |  $$$$$$/|  $$$$$$/  |  $$$$/ "
+echo "  \_______/|__/  |__/        |__/       \______/  \______/    \___/   "
+echo "                                                                      "
+echo
+echo "-----------------------------------------------------------------------------------------------------------------------------------------"
+cat /root/own-installer/art-UserInputs.txt
+#configuration 1
+echo "Nun sind wir im zweiten teil der Instalation. Jetzt kommen sehr viele Fragen. Notier dir am besten die Antworten irgendwo. Das wird jetzt eine Text-Bombe, sorry"
+echo "Wer Windows Dual Booten will, muss nach der Installation os-prober muss den sogenannten Root-Nutzer verwenden mit ’su -’ und mit ’pacman -S os-prober’ os-prober installieren und dann ’grub-mkconfig -o /boot/grub/grub.cfg’ ausführen und kann danach zum User mit ’su BENUTERNAME’ zurückkehren. (Die ’ nicht mitschreiben, danke.)" 
+read -p "Was soll der Hostname fuer das Geraet werden?" HOSTNAME
+read -p "Welches Tastaturlayout? QWERTZ=de-latin1 QWERTY=us " TASTATURLAYOUT
+read -p "Soll ein User erstellt werden? (y/N) (Groß ist der Standart-Wert)" CHOISEUSER
+    if [$CHOISEUSER == y || $CHOISEUSER == Y || $CHOISEUSER == yes || $CHOISEUSER == Yes ||]; then
+        read -p "Wie soll er Heissen?" USERNAME
+        read -p "Was soll sein Passwort werden?" USERPW
+        read -p "Bitte Bestätigen" USERPW2
+            if [USERPW != USERPW2]; then 
+                echo "Die Passwörter stimmen nicht Ueberein. Bitte Script mit neustarten. ((1.) cd /root/own-installer (2.) sh chroot-teil.sh)"
+                exit 1
+            fi
+        useradd -m -G wheel -s /bin/bash $USERNAME
+        chpasswd <<<"$USERNAME:$USERPW"
+        echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+    fi
+read -p "Wie soll das Root-Passwort lauten? (Root ist das eqivalent zum Windows Admin nur mit noch mehr rechten wegen Konsole und so.)" ROOTPW
+    read -p "Bitte Bestätigen" ROOTPW2
+        if [ROOTPW != ROOTPW2]; then 
+            echo "Die Passwörter stimmen nicht Ueberein. Bitte Script mit neustarten. ((1.) cd /root/own-installer (2.) sh chroot-teil.sh)"
+            exit 1
+        fi
+read -p "Wisst du einen Minimalen Install?(y/N) (Ohne DE(KDE Plasma) or WM(Hyprland))" MINIMAL
+    if [$MINIMAL == y || $MINIMAL == Y || $MINIMAL == yes || $MINIMAL == Yes ||]; then
+        cat /root/own-instraller/art-config1.txt
+        echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
+        locale-gen
+        localectl set-locale LANG=de_DE.UTF-8
+        echo "KEYMAP="$TASTATURLAYOUT"" >> /etc/vconsole.conf
+        echo "$HOSTNAME" >> /etc/hostname
+        chpasswd <<<"root:$ROOTPW"  
+        grub-install 
+        grub-mkconfig -o /boot/grub/grub.cfg
+        echo "Die Installation ist fertig. Du kannst jetzt neustarten."
+        exit 1
+    fi
+read -p "Nutzt du NVDIA? (y/N)" NVIDIA
+read -p "Nutzt du AMD oder Intel?" CPUHERSTELLER
+read -p "WM(Hyprland) oder DE(KDE Plasma)?" WMDE
+#config 
+cat /root/own-installer/art-config1.txt
+echo "de_DE.UTF-8"
+locale-gen 
+localectl set-locale LANG=de_DE.UTF-8
+echo "KEYMAP="$TASTATURLAYOUT"" >> /etc/vconsole.conf
+echo "$HOSTNAME" >> /etc/hostname
+chpasswd <<< "root:$ROOTPW"
+#"Treiber"
+if [$NVIDIA == y || $NVIDIA == yes || $NVIDIA == Y || $NVIDIA == Yes || ]; then +
+    pacman -S nvidia nvidia-utils
+fi
+if [$CPUHERSTELLER == Intel]; then
+    pacman -S intel-ucode
+fi
+if [$CPUHERSTELLER == AMD]; then 
+    pacman -S amd-ucode
+fi
+#Programme
+cat /root/own-Installer/art-programs.txt
+if [$WMDE == WM]; then
+    pacman -S wayland hyprland hyprpaper pipewire firefox libreoffice discover nemo wofi xcursor-vanilla-dmz
+    #
+fi
+if [$WMDE == DE]; then 
+    pacman -S plasma firefox libreoffice
+    systemctl enable sddm
+fi
+#Last Config 
+cat /root/own-Installer/art-lastconfig.txt
+pacman -S bluez bluez-utils blueman
+modprobe btubs
+systemctl enable bluetooth.service
+systemctl enable NetworkManager
+grub-install 
+grub-mkconfig -o /boot/grub/grub.cfg
+#ende 
+cat /root/own-installer/art-end.txt
+echo "Die Installation ist fertig und du kannst mit "reboot" neustarten"
+exit 1
