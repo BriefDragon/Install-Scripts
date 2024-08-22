1323d9f5d75c0ecb971945c24f1247c405eb5ca7
BriefDragon <140400090+BriefDragon@users.noreply.github.com>
2024-08-22 17:31:23 +0200

    Update chroot-teil.sh
    Offentlich jetzt Funktional

--- own-installer/chroot-teil.sh
+++ own-installer/chroot-teil.sh
@@ -17,32 +17,32 @@ echo "Wer Windows Dual Booten will, muss nach der Installation os-prober muss de
 read -p "Was soll der Hostname fuer das Geraet werden?" HOSTNAME
 read -p "Welches Tastaturlayout? QWERTZ=de-latin1 QWERTY=us " TASTATURLAYOUT
 read -p "Soll ein User erstellt werden? (y/N) (Groß ist der Standart-Wert)" CHOISEUSER
-    if [$CHOISEUSER==y || $CHOISEUSER==Y || $CHOISEUSER==yes || $CHOISEUSER==Yes ||]; then
+    if [ "$CHOISEUSER"==y || "$CHOISEUSER"==Y || "$CHOISEUSER"==yes || "$CHOISEUSER"==Yes ||]; then
         read -p "Wie soll er Heissen?" USERNAME
         read -p "Was soll sein Passwort werden?" USERPW
         read -p "Bitte Bestätigen" USERPW2
-            if [USERPW != USERPW2]; then 
+            if ["$USERPW" != "USERPW2"]; then 
                 echo "Die Passwörter stimmen nicht Ueberein. Bitte Script mit neustarten. ((1.) cd /root/own-installer (2.) sh chroot-teil.sh)"
                 exit 1
             fi
         useradd -m -G wheel -s /bin/bash $USERNAME
-        chpasswd <<<"$USERNAME:$USERPW"
+        chpasswd <<<""$USERNAME":"$USERPW""
         echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
     fi
 read -p "Wie soll das Root-Passwort lauten? (Root ist das eqivalent zum Windows Admin nur mit noch mehr rechten wegen Konsole und so.)" ROOTPW
     read -p "Bitte Bestätigen" ROOTPW2
-        if [ROOTPW != ROOTPW2]; then 
+        if [ "$ROOTPW" != "$ROOTPW2" ]; then 
             echo "Die Passwörter stimmen nicht Ueberein. Bitte Script mit neustarten. ((1.) cd /root/own-installer (2.) sh chroot-teil.sh)"
             exit 1
         fi
 read -p "Wisst du einen Minimalen Install?(y/N) (Ohne DE(KDE Plasma) or WM(Hyprland))" MINIMAL
-    if [$MINIMAL == y || $MINIMAL == Y || $MINIMAL == yes || $MINIMAL == Yes ||]; then
+    if [ "$MINIMAL" == y || "$MINIMAL" == Y || "$MINIMAL" == yes || "$MINIMAL" == Yes ||]; then
         cat /root/own-instraller/art-config1.txt
         echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
         locale-gen
         localectl set-locale LANG=de_DE.UTF-8
-        echo "KEYMAP="$TASTATURLAYOUT"" >> /etc/vconsole.conf
-        echo "$HOSTNAME" >> /etc/hostname
+        echo "KEYMAP=""$TASTATURLAYOUT""" >> /etc/vconsole.conf
+        echo ""$HOSTNAME"" >> /etc/hostname
         chpasswd <<<"root:$ROOTPW"  
         grub-install 
         grub-mkconfig -o /boot/grub/grub.cfg
@@ -59,24 +59,24 @@ locale-gen
 localectl set-locale LANG=de_DE.UTF-8
 echo "KEYMAP="$TASTATURLAYOUT"" >> /etc/vconsole.conf
 echo "$HOSTNAME" >> /etc/hostname
-chpasswd <<< "root:$ROOTPW"
+chpasswd <<< "root:"$ROOTPW""
 #"Treiber"
-if [$NVIDIA == y || $NVIDIA == yes || $NVIDIA == Y || $NVIDIA == Yes || ]; then +
+if [ "$NVIDIA" == y || "$NVIDIA" == yes || "$NVIDIA" == Y || "$NVIDIA" == Yes || ]; then +
     pacman -S nvidia nvidia-utils
 fi
-if [$CPUHERSTELLER == Intel]; then
+if [ "$CPUHERSTELLER" == Intel ]; then
     pacman -S intel-ucode
 fi
-if [$CPUHERSTELLER == AMD]; then 
+if [ "$CPUHERSTELLER" == AMD ]; then 
     pacman -S amd-ucode
 fi
 #Programme
 cat /root/own-Installer/art-programs.txt
-if [$WMDE == WM]; then
+if [ "$WMDE" == WM ]; then
     pacman -S wayland hyprland hyprpaper pipewire firefox libreoffice discover nemo wofi xcursor-vanilla-dmz
     #
 fi
-if [$WMDE == DE]; then 
+if [ "$WMDE" == DE]; then 
     pacman -S plasma firefox libreoffice
     systemctl enable sddm
 fi
