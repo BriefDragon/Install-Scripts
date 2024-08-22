de3033d6ca8a66c18db4b89aa1a86fa520f2fc06
BriefDragon <140400090+BriefDragon@users.noreply.github.com>
2024-08-22 20:33:35 +0200

    Update Installer.sh

--- own-installer/Installer.sh
+++ own-installer/Installer.sh
@@ -29,6 +29,6 @@ read -p "Bitte jetzt als letztes die Boot Partition auch im selben Schema.: " St
 cat /run/archiso/bootmnt/own-installer/art-programs.txt 
 pacstrap -K /mnt base linux linux-firmware grub efibootmgr nano networkmanager neofetch sof-firmware base-devel git sudo 
 genfstab /mnt > /mnt/etc/fstab
-cp -r ~/Install-Scripts/own-installer /mnt/root #Install-Scripts spaeter tauschen. #Please look what zou write.  ihave revisuted thsi code 3 times and only now i see why the old version was loaded that i have written on the iso. I thought there were a bug but noooo. it loaded the old god daam version and i want to sleep. its 23:50 rn. i cant do this anymore.
+cp -r ~/Install-Scripts/own-installer /mnt/root #Install-Scripts spaeter tauschen. #Please look what you write.  i have revisited this code 3 times and only now i see why the old version was loaded that i have written on the iso. I thought there were a bug but noooo. it loaded the old god daam version and i want to sleep. its 23:50 rn. i cant do this anymore.
 arch-chroot /mnt /bin/bash /root/own-installer/chroot-teil.sh
 #ende dieses Teils 
