3fd62481a0ceccadbb3c3d8a3aaa165b4d95ced8
BriefDragon <140400090+BriefDragon@users.noreply.github.com>
2024-08-22 20:32:02 +0200

    Update Installer.sh
    Same and fixed the cat command at line 11

--- own-installer/Installer.sh
+++ own-installer/Installer.sh
@@ -1,27 +1,26 @@
-#! /bin/bash
-echo "                                                                                                        "
-echo "                                                                                                        "
-echo "  /$$$$$$                      /$$             /$$$$$$                       /$$               /$$ /$$"
-echo "  /$$__  $$                    | $$            |_  $$_/                      | $$              | $$| $$"
-echo " | $$  \ $$  /$$$$$$   /$$$$$$$| $$$$$$$         | $$   /$$$$$$$   /$$$$$$$ /$$$$$$    /$$$$$$ | $$| $$"
-echo " | $$$$$$$$ /$$__  $$ /$$_____/| $$__  $$        | $$  | $$__  $$ /$$_____/|_  $$_/   |____  $$| $$| $$"
-echo " | $$__  $$| $$  \__/| $$      | $$  \ $$        | $$  | $$  \ $$|  $$$$$$   | $$      /$$$$$$$| $$| $$"
-echo " | $$  | $$| $$      | $$      | $$  | $$        | $$  | $$  | $$ \____  $$  | $$ /$$ /$$__  $$| $$| $$"
-echo " | $$  | $$| $$      |  $$$$$$$| $$  | $$       /$$$$$$| $$  | $$ /$$$$$$$/  |  $$$$/|  $$$$$$$| $$| $$"
-echo " |__/  |__/|__/       \_______/|__/  |__/      |______/|__/  |__/|_______/    \___/   \_______/|__/|__/"  
-cat /run/archiso/bootmnt/own-installer/art-input.txt
+#! /bin/bash                                                                                                                                                                                                            
+#  /$$$$$$                      /$$             /$$$$$$                       /$$               /$$ /$$
+#  /$$__  $$                    | $$            |_  $$_/                      | $$              | $$| $$
+# | $$  \ $$  /$$$$$$   /$$$$$$$| $$$$$$$         | $$   /$$$$$$$   /$$$$$$$ /$$$$$$    /$$$$$$ | $$| $$
+# | $$$$$$$$ /$$__  $$ /$$_____/| $$__  $$        | $$  | $$__  $$ /$$_____/|_  $$_/   |____  $$| $$| $$
+# | $$__  $$| $$  \__/| $$      | $$  \ $$        | $$  | $$  \ $$|  $$$$$$   | $$      /$$$$$$$| $$| $$
+# | $$  | $$| $$      | $$      | $$  | $$        | $$  | $$  | $$ \____  $$  | $$ /$$ /$$__  $$| $$| $$
+# | $$  | $$| $$      |  $$$$$$$| $$  | $$       /$$$$$$| $$  | $$ /$$$$$$$/  |  $$$$/|  $$$$$$$| $$| $$
+# |__/  |__/|__/       \_______/|__/  |__/      |______/|__/  |__/|_______/    \___/   \_______/|__/|__/
+# ----------------------------------------------------------------------------------------------------------
+cat ~/Install-Scripts/own-installer/art-input.txt
 echo 
 #Partitionenbeginn jetzt
 lsblk
-read -p "Bitte gebe deine Root-Partition ein wie im folgenden Beispiel (z.b. /dev/sda3). Das Kannst du mit der liste über dir herrauslesen. Sei dir bitte sicher, dass es richtig ist und notier dir auch ruhig die anderen Partitionen." Haupt
+read -p "Bitte gebe deine Root-Partition ein wie im folgenden Beispiel (z.b. /dev/sda3). Das Kannst du mit der liste über dir herrauslesen. Sei dir bitte sicher, dass es richtig ist und notier dir auch ruhig die anderen Partitionen.: " Haupt
     mkfs.ext4 "$Haupt"
     mount "$Haupt" /mnt 
 # Swap 
-read -p "Bitte Swap-partition in dem Schemata von eben eingeben." Tausch
+read -p "Bitte Swap-partition in dem Schemata von eben eingeben.: " Tausch
     mkswap "$Tausch"
     swapon "$Tausch"
 # Boot
-read -p "Bitte jetzt als letztes die Boot Partition auch im selben Schema." Start
+read -p "Bitte jetzt als letztes die Boot Partition auch im selben Schema.: " Start
     mkfs.fat -F 32 "$Start"
     mkdir -p /mnt/boot/efi
     mount "$Start" /mnt/boot/efi
