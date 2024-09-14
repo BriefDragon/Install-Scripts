8b42c078cff52c5fd9e9c3b2bdd5b983c5cd261f
BriefDragon <140400090+BriefDragon@users.noreply.github.com>
2024-09-14 17:03:39 +0200

    Update chroot-teil.sh
    - Fixed a error
    - added cups into the DE Part at the end to install
    - added way to specify the locale

--- own-installer/chroot-teil.sh
+++ own-installer/chroot-teil.sh
@@ -15,34 +15,48 @@ cat /root/own-installer/art-UserInputs.txt
 echo "Nun sind wir im zweiten teil der Instalation. Jetzt kommen sehr viele Fragen. Notier dir am besten die Antworten irgendwo. Das wird jetzt eine Text-Bombe, sorry"
 echo "Wer Windows Dual Booten will, muss nach der Installation os-prober muss den sogenannten Root-Nutzer verwenden mit ’su -’ und mit ’pacman -S os-prober’ os-prober installieren und dann ’grub-mkconfig -o /boot/grub/grub.cfg’ ausführen und kann danach zum User mit ’su BENUTERNAME’ zurückkehren. (Die ’ nicht mitschreiben, danke.)" 
 read -p "Was soll der Hostname fuer das Geraet werden?" HOSTNAME
-read -p "Welches Tastaturlayout? QWERTZ=de-latin1 QWERTY=us " TASTATURLAYOUT
+read -p "Welches Standarttastaturlayout? QWERTZ=de-latin1 QWERTY=us " TASTATURLAYOUT
 read -p "Soll ein User erstellt werden? (y/N) (Groß ist der Standart-Wert)" CHOISEUSER
     if [ "$CHOISEUSER"==y ]; then #|| "$CHOISEUSER"==Y || "$CHOISEUSER"==yes || "$CHOISEUSER"==Yes ||]; then
         read -p "Wie soll er Heissen?: " USERNAME
         read -p "Was soll sein Passwort werden?: " USERPW
         read -p "Bitte Bestätigen: " USERPW2
-            if ["$USERPW" != "USERPW2"]; then 
-                echo "Die Passwörter stimmen nicht Ueberein. Bitte Script mit neustarten. ((1.) cd /root/own-installer (2.) sh chroot-teil.sh)"
-                exit 1
-            fi
+            #if ["$USERPW" != "USERPW2"]; then 
+                #echo "Die Passwörter stimmen nicht Ueberein. Bitte Script mit neustarten. ((1.) cd /root/own-installer (2.) sh chroot-teil.sh)"
+                #exit 1
+            #fi
         useradd -m -G wheel -s /bin/bash $USERNAME
         chpasswd <<<""$USERNAME":"$USERPW""
         echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
     fi
 read -p "Wie soll das Root-Passwort lauten? (Root ist das eqivalent zum Windows Admin nur mit noch mehr rechten wegen Konsole und so.): " ROOTPW
     read -p "Bitte Bestätigen" ROOTPW2
-        if [ "$ROOTPW" != "$ROOTPW2" ]; then 
-            echo "Die Passwörter stimmen nicht Ueberein. Bitte Script mit neustarten. ((1.) cd /root/own-installer (2.) sh chroot-teil.sh)"
-            exit 1
-        fi
+        #if [ "$ROOTPW" != "$ROOTPW2" ]; then 
+            #echo "Die Passwörter stimmen nicht Ueberein. Bitte Script mit neustarten. ((1.) cd /root/own-installer (2.) sh chroot-teil.sh)"
+            #exit 1
+        #fi
 read -p "Wisst du einen Minimalen Install?(y/N) (Ohne DE(KDE Plasma) or WM(Hyprland)): " MINIMAL
     if [ "$MINIMAL" == y ]; then #|| "$MINIMAL" == Y || "$MINIMAL" == yes || "$MINIMAL" == Yes ||]; then
         cat /root/own-instraller/art-config1.txt
-        echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
+        read -p "Welches Land bist du Ansässig? (Deutschland=de ; USA/Kanada=us ; (Japan=jp)WIP): " LC
+        if [ "$LC" == de ]; then
+            echo de_DE.UTF-8 UTF-8 >> /etc/locale.gen
+            locale-gen
+            echo de_DE.UTF-8 >> /etc/locale.conf
+            echo KEYMAP="$TASTATURLAYOUT" >> /etc/vconsole.conf
+        fi
+        if [ "$LC" == us ]; then
+            echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+            locale-gen
+            echo en_US.UTF-8 >> /etc/locale.conf
+        fi
+        #if [ "$LC" == jp ]; then
+            #echo ja_JP.UTF-8 UTF-8 >> /etc/locale.gen
+            #pacman -Sy adobe-source-han-serif-jp-fonts 
+            #echo LANG=ja_JP.UTF-8 >> /etc/locale.conf
+            #pacman -S ibus ibus-anthy
+        #fi 
         locale-gen
-        localectl set-locale LANG=de_DE.UTF-8
-        echo "KEYMAP=""$TASTATURLAYOUT""" >> /etc/vconsole.conf
-        echo ""$HOSTNAME"" >> /etc/hostname
         chpasswd <<<"root:$ROOTPW"  
         grub-install 
         grub-mkconfig -o /boot/grub/grub.cfg
@@ -52,18 +66,32 @@ read -p "Wisst du einen Minimalen Install?(y/N) (Ohne DE(KDE Plasma) or WM(Hyprl
 read -p "Nutzt du NVDIA? (y/N): " NVIDIA
 read -p "Nutzt du AMD oder Intel?: " CPUHERSTELLER
 read -p "WM(Hyprland) oder DE(KDE Plasma)?: " WMDE
+read -p "Welches Land bist du Ansässig? (Deutschland=de ; USA/Kanada=us ; Japan=jp)" LC
 #config 
 cat /root/own-installer/art-config1.txt
-echo "de_DE.UTF-8"
-locale-gen 
-localectl set-locale LANG=de_DE.UTF-8
-echo "KEYMAP="$TASTATURLAYOUT"" >> /etc/vconsole.conf
-echo "$HOSTNAME" >> /etc/hostname
+if [ "$LC" == de ]; then
+    echo de_DE.UTF-8 UTF-8 >> /etc/locale.gen
+    locale-gen
+    echo de_DE.UTF-8 >> /etc/locale.conf
+    echo KEYMAP="$TASTATURLAYOUT" >> /etc/vconsole.conf
+fi
+if [ "$LC" == us ]; then
+    echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+    locale-gen
+    echo en_US.UTF.8 >> /etc/locale.conf
+    echo KEYMAP="$TASTATURLAYOUT" >> /etc/vconsole.conf
+fi
+#if [ "$LC" == jp ]; then
+    #echo ja_JP.UTF-8 UTF-8 >> /etc/locale.gen
+    #WIP because i dont know, how to do the Japanese KEyboard layout thing.
 chpasswd <<< "root:"$ROOTPW""
 #"Treiber"
 pacman -S --noconfirm bluez bluez-utils blueman
 if [ "$NVIDIA" == y ]; then #|| "$NVIDIA" == yes || "$NVIDIA" == Y || "$NVIDIA" == Yes || ]; then +
-    pacman -S --noconfirm nvidia nvidia-utils
+    pacman -S --noconfirm nvidia nvidia-utils libva-nvidia-driver egl-wayland
+    cp /root/own-installer/mkinitcpio.conf /etc/mkinitcpio.conf
+    echo options nvidia_drm modeset=1 dbdev=1 >> /etc/modprobe.d/nvidia.conf
+    mkinitcpio -P
 fi
 if [ "$CPUHERSTELLER" == Intel ]; then
     pacman -S --noconfirm intel-ucode
@@ -78,11 +106,13 @@ if [ "$WMDE" == WM ]; then
     #cp /root/own-installer/dotinstaller.sh /root/home/$USERNAME
 fi
 if [ "$WMDE" == DE ]; then 
-    pacman -S --noconfirm sddm plasma firefox libreoffice
+    pacman -S --noconfirm sddm plasma firefox libreoffice konsole dolphin system-config-printer cups cups-pdf
     systemctl enable sddm
+    systemctl enable cups
 fi
 #Last Config 
 cat /root/own-Installer/art-lastconfig.txt
+if [  ]
 modprobe btubs
 systemctl enable bluetooth.service
 systemctl enable NetworkManager
@@ -91,4 +121,5 @@ grub-mkconfig -o /boot/grub/grub.cfg
 #ende 
 cat /root/own-installer/art-end.txt
 echo "Die Installation ist fertig und du kannst mit ’reboot’ neustarten. Wenn du Hyprland ausgewählt hast, kannst du die Installation der dotfiles nach dem reboot durch 1. ’sh dotinstaller.sh’ starten. (Anmerkung: Work in progress)"
+rm /root/own-installer
 exit 1
